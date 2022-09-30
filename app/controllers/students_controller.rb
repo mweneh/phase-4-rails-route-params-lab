409d2accb530e0ac1de8ecfb10a6aeeda208853e
David <davidongaroh@gmail.com>
2022-09-30 15:38:26 +0300

    solved

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:first_name]
+                 Student.find_by(first_name:params[:first_name].capitalize)
+    elsif params[:last_name]
+          Student.find_by(last_name:params[:last_name].capitalize)
+    else
+                 Student.all
+               end    
     render json: students
+  end
+  
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
